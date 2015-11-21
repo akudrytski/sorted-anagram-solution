6343ab3d7bb18e762a6a0db36026a37e7fdf3ec9
Andrei Kudrytski <akudrytski@gmail.com>
2015-11-21 11:07:37 +0300

    Files updated

---
+++ SortedAnagramSolution/SortedAnagramSolution/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace SortedAnagramSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter [a-z] string less or equal to 25 characters.");
+            var initialSource = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(initialSource))
+            {
+                Console.WriteLine("String is empty.");
+                Console.ReadKey();
+                return;
+            }
+
+            if (initialSource.Length > 25)
+            {
+                Console.WriteLine("String is too long.");
+                Console.ReadKey();
+                return;
+            }
+
+            var source = initialSource.ToLower();
+            if (!Regex.IsMatch(source, @"^[a-z]+$"))
+            {
+                Console.WriteLine("String contains invalid values.");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Please select the number of times to execute the procedure.");
+            var initialTimesToRun = Console.ReadLine();
+            long timesToRun = 0;
+            if (!long.TryParse(initialTimesToRun, out timesToRun))
+            {
+                Console.WriteLine("Invalid times to run number.");
+                Console.ReadKey();
+                return;
+            }
+
+            var stopWatch = new Stopwatch();
+
+            var rankCalculator = new RankCalculator();
+            var result = 0d;
+            for (var index = 0L; index < timesToRun; index++)
+            {
+                stopWatch.Start();
+                result = rankCalculator.GetRank(source);
+                stopWatch.Stop();
+            }
+
+            Console.WriteLine("Source:{0}, Times executed:{1}, Average execution time(ms):{2}; Result:{3}", source,timesToRun, (double)stopWatch.ElapsedMilliseconds/timesToRun, result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SortedAnagramSolution/SorterAnagramSolution.Tests/PermutationsHelperTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using SortedAnagramSolution;
+
+namespace SorterAnagramSolution.Tests
+{
+    [TestFixture]
+    public class PermutationsHelperTests
+    {
+        [TestCase(0, 1d)]
+        [TestCase(1, 1d)]
+        [TestCase(2,2d)]
+        [TestCase(3,6d)]
+        [TestCase(10, 3628800d)]
+        public void FactorialReturnsCorrectValueWhenInvokedAgainstPositiveIntegerOrZero(int source, double expectedValue)
+        {
+            //Act
+            var result = PermutationsHelper.GetFactorial(source);
+
+            //Assert
+            Assert.AreEqual(expectedValue, result, 0.0000000001);
+        }
+
+        [TestCase("", 0)]
+        [TestCase("a", 1)]
+        [TestCase("abc", 6)]
+        [TestCase("abcd", 24)]
+        [TestCase("aabc", 12)]
+        [TestCase("aaabc", 20)]
+        [TestCase("aaabc", 20)]
+        [TestCase("aabbcc", 90)]
+        public void PermutationsCountReturnsCorrectValueWhenInvoked(string source, double expectedValue)
+        {
+            //Arrange
+            var charArray = source.ToCharArray();
+
+            //Act
+            var result = PermutationsHelper.GetPermutationsCount(charArray);
+
+            //Assert
+            Assert.AreEqual(expectedValue, result, 0.0000000001);
+        }
+    }
+}
